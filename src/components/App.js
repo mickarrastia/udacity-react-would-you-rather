0b71a37d63702c4dd8b931795356df9987040945
mickarrastia <michael.arrastia@gmail.com>
2018-05-30 17:15:00 +0100

    Add loading of initial data
    This includes:
    - actions and reducers to handle dispatch to the store
    - thunk middleware to support asynchronous dispatch of data retrieval
      functions
    - logger middleware to log the actions and the state following their
      dispatch

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import {connect} from 'react-redux'
+import {handleInitialData} from '../actions/shared'
+
+class App extends Component {
+  componentDidMount() {
+    this.props.dispatch(handleInitialData())
+  }
+  render() {
+    return (
+      <div>Hello</div>
+    )
+  }
+}
+
+export default connect()(App)
--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './components/App'
+import {createStore} from 'redux'
+import {Provider} from 'react-redux'
+import reducer from './reducers'
+import middleware from './middleware'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+const store = createStore(reducer, middleware)
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App/>
+  </Provider>, document.getElementById('root'));
